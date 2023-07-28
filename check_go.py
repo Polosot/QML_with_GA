fb07de477734d8e54b89995ca072a4d7f1651e03
Maksym Sarana <maksym@verbit.ai>
2023-07-28 13:40:19 +0300

    genetic optimizer

--- check_go.py
+++ check_go.py
@@ -6,8 +6,9 @@ if __name__ == '__main__':
     data = init_data()
     go = GeneticOptimizer(data=data, model_func=loss_function, num_cubits=12)
 
-    losses = go.run_model(go.population)
-    p = go.selection(go.population, losses)
-    p = go.cross_over(p)
+    # losses = go.run_model(go.population)
+    # p = go.selection(go.population, losses)
+    # p = go.cross_over(p)
+    # p = go.mutate(p, 0.0001)
 
-    print(p)
+    go.fit()
