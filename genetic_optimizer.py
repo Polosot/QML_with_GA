c8b6563237d386e1e45335128e237c4b9a49461e
Maksym Sarana <maksym@verbit.ai>
2023-07-28 12:29:38 +0300

    init pop

--- genetic_optimizer.py
+++ genetic_optimizer.py
@@ -1,5 +1,23 @@
 
 
+class GeneticOptimizer:
+
+    def __init__(self, G, model_class, population_size=10):
+        self.G = G
+        self.ModelClass = model_class
+        self.population_size = population_size
+        self.population = self.init_population()
+
+    def init_model_qweights(self):
+        pass
+
+    def init_population(self):
+        res = []
+        for _ in range(self.population_size):
+            res.append(self.init_model_qweights())
+
+        return res
+
 
 
 if __name__ == '__main__':
