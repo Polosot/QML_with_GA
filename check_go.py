e6d339bb793d6aeffba41b96929a4d81a485f9aa
Maksym Sarana <maksym@verbit.ai>
2023-07-28 15:08:09 +0300

    plot

--- check_go.py
+++ check_go.py
@@ -1,14 +1,17 @@
 from main import loss_function, init_data, qnode
 from genetic_optimizer import GeneticOptimizer
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
     data = init_data()
-    go = GeneticOptimizer(model=qnode, data=data, loss_func=loss_function, num_cubits=12)
+    go = GeneticOptimizer(model=qnode, data=data, loss_func=loss_function, num_cubits=12, coef_bits=36,
+                          mutation_prob=0.001, population_size=100, G=300)
 
-    # losses = go.run_model(go.population)
-    # p = go.selection(go.population, losses)
-    # p = go.cross_over(p)
-    # p = go.mutate(p, 0.0001)
+    average_cost_list = go.fit()
 
-    go.fit()
+    plt.plot(average_cost_list)
+    plt.xlabel("Epochs")
+    plt.ylabel("Average loss among population")
+    plt.title("Genetic algorithm")
+    plt.show()
